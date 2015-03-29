bcc29a3b8084c456d3eab8b083826d3b3c47d487
emRitchie Vincent/em <emrfcvincent@gmail.com/em>
2015-03-29 23:38:56 +0100

    Updated API request

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,31 @@
 angular.module('rivv.controllers', [])
 
 
-.controller('homeForm', function ($scope, $ionicLoading, $state) {
+.controller('homeForm', function ($scope, $ionicLoading, $state, $http) {
 
     $scope.update = function (search) {
         //        alert(search);
         //        alert($scope.select.platform);
         $ionicLoading.show({
-            template: '<ion-spinner class="spinner-energized"></ion-spinner>'
-        })
-        var output = $.ajax({
-            url: 'https://videogamesrating.p.mashape.com/get.php?count=1&game=' + search + '',
-            type: 'GET',
+                template: '<ion-spinner class="spinner-energized"></ion-spinner>'
+            })
+
+        var req = {
+            method: 'GET',
+            url: 'https://videogamesrating.p.mashape.com/get.php?count=1&game=god+of+war',
+            headers: {
+                'X-Mashape-Authorization': 'H4ldhpG7ZgmshKoX1vWY188hF2fnp1yCl3yjsngteXB6yw6uOH'
+            },
             data: {},
-            datatype: 'json',
-            success: function (data) {
-                $ionicLoading.hide()
-                $state.go('app.details');
-                $scope.score = data[0].score;
-            },
-            error: function (err) {
-                alert(err);
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "H4ldhpG7ZgmshKoX1vWY188hF2fnp1yCl3yjsngteXB6yw6uOH");
-            }
+            datatype: 'json'
+        }
+
+        $http(req).success(function (data) {
+            $ionicLoading.hide()
+            $state.go('app.details');
+            $scope.score = data[0].score;
+        }).error(function (err) {
+            alert(err);
         });
     };
 });
